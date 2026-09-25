158611d876e3a6f3939e84b001e47ad45c3de91c
InSuns21 <senkonoshine@yahoo.co.jp>
2026-09-26 01:46:01 +0900

    Use MapLibre namespace import

--- src/templates/chronicle/ThematicMap.tsx
+++ src/templates/chronicle/ThematicMap.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useRef, type CSSProperties } from 'react'
-import maplibregl from 'maplibre-gl'
+import * as maplibregl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import { findMapDefinition } from '../../maps/registry'
 

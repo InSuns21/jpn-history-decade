86894ae708030588d9111a92babcb1f9471d82ab
InSuns21 <senkonoshine@yahoo.co.jp>
2026-09-25 22:08:01 +0900

    JH00: move not-found presentation into template

---
+++ src/templates/chronicle/NotFoundTemplate.tsx
@@ -0,0 +1,9 @@
+export function NotFoundTemplate() {
+  return (
+    <section className="page-width not-found">
+      <p>404</p>
+      <h1>ページが見つかりません</h1>
+      <a className="button button--secondary" href="#/">年代一覧へ戻る</a>
+    </section>
+  )
+}

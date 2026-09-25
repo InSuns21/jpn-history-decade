e8080cd64229b586e5baca1982c8ef1d92bc769e
InSuns21 <senkonoshine@yahoo.co.jp>
2026-09-25 22:27:14 +0900

    JH01: render inline source references

--- src/templates/chronicle/LinkedText.tsx
+++ src/templates/chronicle/LinkedText.tsx
@@ -3,30 +3,42 @@ import { Fragment, type ReactNode } from 'react'
 interface LinkedTextProps {
   text: string
   routeKey: string
+  sourceIds?: string[]
 }
 
-const TERM_LINK = /\[\[term:([a-z0-9-]+)\|([^\]]+)\]\]/g
+const INLINE_LINK = /\[\[term:([a-z0-9-]+)\|([^\]]+)\]\]|\[@([a-z0-9-]+)\]/g
 
-export function LinkedText({ text, routeKey }: LinkedTextProps) {
+export function LinkedText({ text, routeKey, sourceIds = [] }: LinkedTextProps) {
   const nodes: ReactNode[] = []
   let cursor = 0
   let match: RegExpExecArray | null
 
-  while ((match = TERM_LINK.exec(text)) !== null) {
-    const [raw, termId, label] = match
+  while ((match = INLINE_LINK.exec(text)) !== null) {
+    const [raw, termId, label, sourceId] = match
     const start = match.index
 
     if (start > cursor) nodes.push(text.slice(cursor, start))
 
-    nodes.push(
-      <a
-        className="glossary-link"
-        href={'#/period/' + routeKey + '/terms/' + termId}
-        key={termId + '-' + start}
-      >
-        {label}
-      </a>,
-    )
+    if (termId) {
+      nodes.push(
+        <a
+          className="glossary-link"
+          href={'#/period/' + routeKey + '/terms/' + termId}
+          key={'term-' + termId + '-' + start}
+        >
+          {label}
+        </a>,
+      )
+    } else if (sourceId) {
+      const sourceNumber = sourceIds.indexOf(sourceId) + 1
+      nodes.push(
+        <sup className="source-ref" key={'source-' + sourceId + '-' + start}>
+          <a href={'#source-' + sourceId} aria-label={'出典 ' + sourceNumber}>
+            [{sourceNumber}]
+          </a>
+        </sup>,
+      )
+    }
 
     cursor = start + raw.length
   }

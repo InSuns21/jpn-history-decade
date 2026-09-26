66e01826f3cca26a6a97d2046a921aa14eed2012
InSuns21 <senkonoshine@yahoo.co.jp>
2026-09-26 17:28:24 +0900

    fix: register A3 map in content compiler registry

--- src/maps/registry.ts
+++ src/maps/registry.ts
@@ -2,11 +2,13 @@ import { validateMapData } from './audit/validateData.ts'
 import { validateMapStyle } from './audit/validateStyle.ts'
 import { bakumatsuEarlyContact1853Map } from './definitions/bakumatsuEarlyContact1853.ts'
 import { treatyPortsTransport1859Map } from './definitions/treatyPortsTransport1859.ts'
+import { boshinWar1868Map } from './definitions/boshinWar1868.ts'
 import type { HistoricalMapDefinition } from './schema.ts'
 
 export const mapDefinitions: HistoricalMapDefinition[] = [
   bakumatsuEarlyContact1853Map,
   treatyPortsTransport1859Map,
+  boshinWar1868Map,
 ]
 
 export function findMapDefinition(id: string) {
